20f4c23b31943785d2595ee0fc806836feaf9670
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-12-27 10:42:57 +0300

    added validations &relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,11 @@
 class Customer < ApplicationRecord
+    belongs_to :admin
+    validates :name, presence:true
+    validates :email, presence:true, uniqueness:true
+    has_secure_password
+    validates :password,
+    length: { minimum: 6 },
+     if:  -> { new_record? || !password.nil? }
+
+    validates :admin_id, presence:true
 end
