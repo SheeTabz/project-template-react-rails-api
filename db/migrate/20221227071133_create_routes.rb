aa28c42b0da80a1c115a4c2de8dc9730faa5ed88
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-12-27 11:26:15 +0300

    updated route table

--- db/migrate/20221227071133_create_routes.rb
+++ db/migrate/20221227071133_create_routes.rb
@@ -1,7 +1,10 @@
 class CreateRoutes < ActiveRecord::Migration[6.1]
   def change
     create_table :routes do |t|
-
+      t.integer :id
+      t.string :From
+      t.string :To
+      t.string :Price
       t.timestamps
     end
   end
