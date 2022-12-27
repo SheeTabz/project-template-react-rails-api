d074e1536a5aa40f20ab65c2c38ade0e34463d84
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-12-27 10:36:38 +0300

    updated admin controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,17 @@
 class AdminController < ApplicationController
+    def index
+
+    end
+    def show
+
+    end
+    def update
+
+    end
+    def destroy
+
+    end
+    def create
+        
+    end
 end
