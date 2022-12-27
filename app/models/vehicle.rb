d9590322ea74d5ca99096dcc39707032ed28cd8b
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-12-27 10:45:14 +0300

    added validations and relationships

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,8 @@
 class Vehicle < ApplicationRecord
+    has_many :routes
+    validates :route_id, presence:true
+    validates :seat_id, presence:true
+    validates :sacco_id, presence:true
+    validates :image
+    validates :time
 end
